e8dd604adc9c55551e0a6b65142b00aed0036826
Endative <endative@gmail.com>
2022-11-19 12:09:02 +0000

    Initial commit

---
+++ src/main/java/net/endative/plateia/Plateia.java
@@ -0,0 +1,14 @@
+package net.endative.plateia;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Plateia implements ModInitializer {
+	public static final String MOD_ID = "plateia";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/endative/plateia/PlateiaClient.java
@@ -0,0 +1,10 @@
+package net.endative.plateia;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PlateiaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
